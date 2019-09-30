c271244f82e2aa5ab3925105c159415792e3c970
aljica <aljica@debian>
2019-09-30 21:45:38 +0200

    fix mock

--- lab2/mock/ViewControl.java
+++ lab2/mock/ViewControl.java
@@ -2,12 +2,13 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class ViewControl extends JFrame /*implements ActionListener*/ {
+class ViewControl extends JFrame {
 
   private Boardgame game;
   private int size;
   private Square[][] board; // Square är subklass till JButton (tänk MyButton)
   private JLabel mess = new JLabel();
+  JPanel thePanel = new JPanel();
 
   public static void main(String[] u) {
     Boardgame game = new NewGame(); // See https://stackoverflow.com/questions/16750772/instantiating-interfaces-in-java
@@ -22,30 +23,11 @@ class ViewControl extends JFrame /*implements ActionListener*/ {
     JFrame f = new JFrame();
     f.setSize(700, 700);
 
-    JPanel thePanel = new JPanel();
     thePanel.setLayout(null); // Could use GridLayout, maybe?
-
     f.add(thePanel);
 
     // Creating all the buttons.
-    board = new Square[size][size];
-
-    for (int i=0; i<size; i++) {
-      for (int j=0; j<size; j++) {
-        Square sq = new Square(i, j, " ");
-        sq.addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent e) {
-            //sq.sendData(); // Method to perform move in model class.
-            game.move(sq.i, sq.j);
-            updateStatus();
-            updateMessage();
-          }
-        });
-        thePanel.add(sq);
-        sq.setBounds(j*100+100, i*100+100, 100, 100); // j is column-based, i is row-based
-        board[i][j] = sq; // Storing the square in our board.
-      }
-    }
+    createButtons();
 
     // Updating game status.
     updateStatus();
@@ -58,24 +40,39 @@ class ViewControl extends JFrame /*implements ActionListener*/ {
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
 
+  void createButtons() {
+    board = new Square[size][size];
+
+    for (int i=0; i<size; i++) {
+      for (int j=0; j<size; j++) {
+        Square sq = new Square(i, j, " ");
+        sq.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            game.move(sq.i, sq.j);
+            updateStatus();
+            updateMessage();
+          }
+        });
+        thePanel.add(sq);
+        sq.setBounds(j*100+100, i*100+100, 100, 100); // j is column-based, i is row-based
+        board[i][j] = sq; // Storing the square in our board.
+      }
+    }
+  }
+
   void updateStatus() {
     String status;
     for (int i=0; i<size; i++) {
       for (int j=0; j<size; j++) {
         status = game.getStatus(i, j);
-        //status = "hi";
         board[i][j].setText(status);
       }
     }
-    // Get statuses, update button locations in matrix.
   }
 
   void updateMessage() {
     String msg = game.getMessage();
     mess.setText(msg);
-    //String msg = "hi";
-    //return msg;
-    // Updating the message.
   }
 
 }
