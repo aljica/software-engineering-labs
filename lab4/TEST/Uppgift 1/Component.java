1ed77ff19ce4adb41595d765d9d7a9e464ad7e87
aljica <aljica@debian>
2019-10-01 14:42:26 +0200

    add comment to Component, in case

--- lab4/TEST/Uppgift 1/Component.java
+++ lab4/TEST/Uppgift 1/Component.java
@@ -6,4 +6,9 @@ abstract class Component {
   public abstract int getWeight();
   public abstract String toString();
 
+  // Vi kan ha detta också. Förhindrar då Leaf från att ärva add() (?).
+  /*public astract void add() {
+    throw new UnsupportedOperationException();
+  }*/
+
 }
