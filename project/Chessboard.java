67d071cba0170b45c3076813e9f4c6aa0e510a77
aljica <aljica@debian>
2019-10-07 17:09:55 +0200

    begin work on chess board

---
+++ project/Chessboard.java
@@ -0,0 +1,15 @@
+public class Chessboard implements Boardgame {
+
+  public boolean move(int i, int j) {
+    return true;
+  }
+
+  public String getStatus(int i, int j) {
+    return "Works"
+  }
+
+  public String getMessage() {
+    return "OK";
+  }
+
+}
