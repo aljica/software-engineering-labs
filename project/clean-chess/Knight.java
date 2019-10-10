07d58a06e83b9a7fe7dc9ff434f57091bfec7f2b
aljica <aljica@debian>
2019-10-10 18:56:38 +0200

    implemented knight movement

--- project/clean-chess/Knight.java
+++ project/clean-chess/Knight.java
@@ -9,139 +9,158 @@ public class Knight extends Piece {
     }
   }
 
-  public void getJwhenIequalsZero(Piece[][] board) {
-    if (this.j == 0) {
-      this.oneDownTwoRight();
-      this.twoDownOneRight();
-    }
-    else if (this.j == 1) {
-
-    }
-    else if (1 < this.j < 6) {
-
-    }
-    else if (this.j == 6) {
-
-    }
-    else if (this.j == 7) {
-
-    }
-    else {
-      // This is temporary (?)
-      System.out.println("FATAL ERROR");
-      System.exit(0);
-    }
-  }
-
   public void checkPossibleSquares(Piece[][] board) {
-    // Check conditions and create appropriate sub-functions
-    // to handle different possibilities
-    switch(this.i) {
-      case 0:
-        this.getJwhenIequalsZero(board);
-      case 1:
-        this.getJwhenIequalsOne(board);
+    if (this.i == 0) {
+      // Knight is at (0, j)
+      if (this.j == 0) {
+        // Knight is at (0, 0)
+        this.allPossibleManeuvers(board, 1, 2); // 1 down, 2 right
+        this.allPossibleManeuvers(board, 2, 1);
+      }
+      else if (this.j == 1) {
+        this.allPossibleManeuvers(board, 2, 1); // 2 down, 1 right
+        this.allPossibleManeuvers(board, 1, 2); // 1 down, 2 right
+        this.allPossibleManeuvers(board, 2, -1); // 2 down, 1 left
+      }
+      else if (1 < this.j && this.j < 6) {
+        this.allPossibleManeuvers(board, 2, 1); // 2 down, 1 right
+        this.allPossibleManeuvers(board, 1, 2);
+        this.allPossibleManeuvers(board, 1, -2);
+        this.allPossibleManeuvers(board, 2, -1);
+      }
+      else if (this.j == 6) {
+        this.allPossibleManeuvers(board, 2, 1); // 2 down, 1 right
+        this.allPossibleManeuvers(board, 1, -2); // 1 down, 2 left
+        this.allPossibleManeuvers(board, 2, -1);
+      }
+      else if (this.j == 7) {
+        this.allPossibleManeuvers(board, 1, -2);
+        this.allPossibleManeuvers(board, 2, -1);
+      }
+    }
+    else if (this.i == 1) {
+      if (this.j == 0) {
+        this.allPossibleManeuvers(board, -1, 2);
+        this.allPossibleManeuvers(board, 1, 2);
+        this.allPossibleManeuvers(board, 2, 1);
+      }
+      else if (this.j == 1) {
+        this.allPossibleManeuvers(board, -1, 2);
+        this.allPossibleManeuvers(board, 2, -1);
+        this.allPossibleManeuvers(board, 2, 1);
+        this.allPossibleManeuvers(board, 1, 2);
+      }
+      else if (1 < this.j && this.j < 6) {
+        this.allPossibleManeuvers(board, -1, -2);
+        this.allPossibleManeuvers(board, -1, 2);
+        this.allPossibleManeuvers(board, 1, -2);
+        this.allPossibleManeuvers(board, 2, -1);
+        this.allPossibleManeuvers(board, 2, 1);
+        this.allPossibleManeuvers(board, 1, 2);
+      }
+      else if (this.j == 6) {
+        this.allPossibleManeuvers(board, -1, -2);
+        this.allPossibleManeuvers(board, 1, -2);
+        this.allPossibleManeuvers(board, 2, -1);
+        this.allPossibleManeuvers(board, 2, 1);
+      }
+      else if (this.j == 7) {
+        this.allPossibleManeuvers(board, -1, -2);
+        this.allPossibleManeuvers(board, 1, -2);
+        this.allPossibleManeuvers(board, 2, -1);
+      }
+    }
+    else if (this.i == 6) {
+      if (this.j == 0) {
+        this.allPossibleManeuvers(board, -1, 2);
+        this.allPossibleManeuvers(board, -2, 1);
+        this.allPossibleManeuvers(board, 1, 2);
+      }
+      else if (this.j == 1) {
+        this.allPossibleManeuvers(board, -2, -1);
+        this.allPossibleManeuvers(board, -2, 1);
+        this.allPossibleManeuvers(board, -1, 2);
+        this.allPossibleManeuvers(board, 1, 2);
+      }
+      else if (1 < this.j && this.j < 6) {
+        this.allPossibleManeuvers(board, -1, -2);
+        this.allPossibleManeuvers(board, -2, -1);
+        this.allPossibleManeuvers(board, -2, 1);
+        this.allPossibleManeuvers(board, -1, 2);
+        this.allPossibleManeuvers(board, 1, -2);
+        this.allPossibleManeuvers(board, 1, 2);
+      }
+      else if (this.j == 6) {
+        this.allPossibleManeuvers(board, -1, -2);
+        this.allPossibleManeuvers(board, -2, -1);
+        this.allPossibleManeuvers(board, -2, 1);
+        this.allPossibleManeuvers(board, 1, -2);
+      }
+      else if (this.j == 7) {
+        this.allPossibleManeuvers(board, -2, -1);
+        this.allPossibleManeuvers(board, -1, -2);
+        this.allPossibleManeuvers(board, 1, -2);
+      }
+    }
+    else if (this.i == 7) {
+      if (this.j == 0) {
+        this.allPossibleManeuvers(board, -2, 1);
+        this.allPossibleManeuvers(board, -1, 2);
+      }
+      else if (this.j == 1) {
+        this.allPossibleManeuvers(board, -2, -1);
+        this.allPossibleManeuvers(board, -2, 1);
+        this.allPossibleManeuvers(board, -1, 2);
+      }
+      else if (1 < this.j && this.j < 6) {
+        this.allPossibleManeuvers(board, -1, -2);
+        this.allPossibleManeuvers(board, -2, -1);
+        this.allPossibleManeuvers(board, -1, 2);
+        this.allPossibleManeuvers(board, -2, 1);
+      }
+      else if (this.j == 6) {
+        this.allPossibleManeuvers(board, -1, -2);
+        this.allPossibleManeuvers(board, -2, -1);
+        this.allPossibleManeuvers(board, -2, 1);
+      }
+      else if (this.j == 7) {
+        this.allPossibleManeuvers(board, -1, -2);
+        this.allPossibleManeuvers(board, -2, -1);
+      }
     }
   }
 
   public void addAllPossibleSquares(Piece[][] board) {
     // Up/down from white's point of view.
-    this.twoUpOneLeft(board);
-    this.twoUpOneRight(board);
-    this.twoDownOneLeft(board);
-    this.twoDownOneRight(board);
+    this.allPossibleManeuvers(board, -2, -1);
+    this.allPossibleManeuvers(board, -2, 1);
+    this.allPossibleManeuvers(board, 2, -1);
+    this.allPossibleManeuvers(board, 2, 1);
 
-    this.oneUpTwoLeft(board);
-    this.oneUpTwoRight(board);
-    this.oneDownTwoLeft(board);
-    this.oneDownTwoRight(board);
+    this.allPossibleManeuvers(board, -1, -2);
+    this.allPossibleManeuvers(board, -1, 2);
+    this.allPossibleManeuvers(board, 1, -2);
+    this.allPossibleManeuvers(board, 1, 2);
   }
 
-  public void oneDownTwoRight(Piece[][] board) {
-    // Moving two steps down, one to the right.
-    Piece piece = board[this.i + 1][this.j + 2];
+  public void allPossibleManeuvers(Piece[][] board, int a, int b) {
+    // 1 down 2 right: a = 1, b = 2
+    // 1 down 2 left: 1, -2
+    // 1 up, 2 right: -1, 2
+    // 1 up, 2 left: -1, -2
+    // 2 down, 1 right: 2, 1
+    // 2 down, 1 left: 2, -1
+    // 2 up, 1 right: -2, 1
+    // 2 up, 1 left: -2, -1
+    Piece piece = board[this.i + a][this.j + b];
     if (piece != null) {
       // If it's an opposite colored piece, we can capture it
       if ((this.value % 2) != (piece.getIdentifier() % 2)) {
-        this.addMove(1, 2);
+        this.addMove(a, b);
       }
     }
-  }
-
-  public void oneDownTwoLeft(Piece[][] board) {
-    // Moving two steps down, one to the right.
-    Piece piece = board[this.i + 1][this.j - 2];
-    if (piece != null) {
-      // If it's an opposite colored piece, we can capture it
-      if ((this.value % 2) != (piece.getIdentifier() % 2)) {
-        this.addMove(1, -2);
-      }
-    }
-  }
-
-  public void oneUpTwoRight(Piece[][] board) {
-    // Moving two steps down, one to the right.
-    Piece piece = board[this.i - 1][this.j + 2];
-    if (piece != null) {
-      // If it's an opposite colored piece, we can capture it
-      if ((this.value % 2) != (piece.getIdentifier() % 2)) {
-        this.addMove(-1, 2);
-      }
-    }
-  }
-
-  public void oneUpTwoLeft(Piece[][] board) {
-    // Moving two steps down, one to the right.
-    Piece piece = board[this.i - 1][this.j - 2];
-    if (piece != null) {
-      // If it's an opposite colored piece, we can capture it
-      if ((this.value % 2) != (piece.getIdentifier() % 2)) {
-        this.addMove(-1, -2);
-      }
-    }
-  }
-
-  public void twoDownOneRight(Piece[][] board) {
-    // Moving two steps down, one to the right.
-    Piece piece = board[this.i + 2][this.j + 1];
-    if (piece != null) {
-      // If it's an opposite colored piece, we can capture it
-      if ((this.value % 2) != (piece.getIdentifier() % 2)) {
-        this.addMove(2, 1);
-      }
-    }
-  }
-
-  public void twoDownOneLeft(Piece[][] board) {
-    // Moving two steps down, one to the left.
-    Piece piece = board[this.i + 2][this.j - 1];
-    if (piece != null) {
-      // If it's an opposite colored piece, we can capture it
-      if ((this.value % 2) != (piece.getIdentifier() % 2)) {
-        this.addMove(2, -1);
-      }
-    }
-  }
-
-  public void twoUpOneRight(Piece[][] board) {
-    // Moving two steps up, one to the right.
-    Piece piece = board[this.i - 2][this.j + 1];
-    if (piece != null) {
-      // If it's an opposite colored piece, we can capture it
-      if ((this.value % 2) != (piece.getIdentifier() % 2)) {
-        this.addMove(-2, 1);
-      }
-    }
-  }
-
-  public void twoUpOneLeft(Piece[][] board) {
-    // Moving two steps up, one to the left.
-    Piece piece = board[this.i - 2][this.j - 1];
-    if (piece != null) {
-      // If it's an opposite colored piece, we can capture it
-      if ((this.value % 2) != (piece.getIdentifier() % 2)) {
-        this.addMove(-2, -1);
-      }
+    else {
+      this.addMove(a, b);
     }
   }
 
