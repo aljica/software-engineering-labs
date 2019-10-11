df7a5346344215c4598d154beeaea26ab87aa948
aljica <aljica@debian>
2019-10-11 21:29:22 +0200

    clean branch

--- project/CheckeredBoardgame.java
+++
@@ -1,7 +0,0 @@
-interface CheckeredBoardgame {
-
-  public boolean move(int i, int j); // true om draget gick bra, false annars.
-  public String getStatus(int i, int j); // returnera innehåll på ruta (i, j)
-  public String getMessage(); // OK eller felmeddelande avseende föregående drag
-
-}
--- project/new-chess/Boardgame.java
+++
@@ -1,8 +0,0 @@
-public interface Boardgame {
-
-  public void select(int i, int j);
-  public void drop(int i, int j);
-  public Piece getStatus(int i, int j);
-  public String getMessage();
-
-}
