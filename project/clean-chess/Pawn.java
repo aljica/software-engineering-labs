2eea3fcfaa232d616176a35bee16fc895508e14a
aljica <aljica@debian>
2019-10-11 14:38:00 +0200

    cleaned up code for pawn and rook

--- project/clean-chess/Pawn.java
+++ project/clean-chess/Pawn.java
@@ -17,46 +17,38 @@ public class Pawn extends Piece {
     }
   }
 
-  public void diagonalCapturesForEdgePawns(Piece[][] board) {
-    if (this.j == 0) {
-      // If pawn on left edge of board, only check right diagonal.
-      this.checkRightDiagonal(board);
-    }
-    else {
-      this.checkLeftDiagonal(board);
+  public void addDiagonalSquareIfOpposite(Piece[][] board, int a, int b) {
+    if (this.destinationSquareHasOppositeColor(board, a, b)) {
+      this.addMove(a, b);
     }
   }
 
   public void checkRightDiagonal(Piece[][] board) {
     if (this.isWhite) {
-      if (this.destinationSquareHasOppositeColor(board, this.i-1, this.j+1)) {
-        this.addMove(this.i - 1, this.j + 1);
-      }
+      this.addDiagonalSquareIfOpposite(board, this.i-1, this.j+1);
     }
     else {
-      if (this.destinationSquareHasOppositeColor(board, this.i+1, this.j+1)) {
-        this.addMove(this.i + 1, this.j + 1);
-      }
+      this.addDiagonalSquareIfOpposite(board, this.i+1, this.j+1);
     }
   }
 
   public void checkLeftDiagonal(Piece[][] board) {
     if (this.isWhite) {
-      if (this.destinationSquareHasOppositeColor(board, this.i-1, this.j-1)) {
-        this.addMove(this.i - 1, this.j - 1);
-      }
+      this.addDiagonalSquareIfOpposite(board, this.i-1, this.j-1);
     }
     else {
-      if (this.destinationSquareHasOppositeColor(board, this.i+1, this.j-1)) {
-        this.addMove(this.i + 1, this.j - 1);
-      }
+      this.addDiagonalSquareIfOpposite(board, this.i+1, this.j-1);
     }
   }
 
   public void checkDiagonalCaptures(Piece[][] board) {
     // Check if the pawn is an edge pawn
-    if (this.j == 0 || this.j == 7) {
-      this.diagonalCapturesForEdgePawns(board);
+    if (this.j == 0) {
+      this.checkRightDiagonal(board);
+      return;
+    }
+    else if (this.j == 7) {
+      this.checkLeftDiagonal(board);
       return;
     }
 
@@ -66,25 +58,20 @@ public class Pawn extends Piece {
   }
 
   public void updateLegalMoves(Piece[][] board) {
-    if (this.firstMove) {
-      if (this.isWhite) {
-        // Add the square directly in front, and 2 steps in front.
-        this.addSquareIfEmpty(board, this.i - 1, this.j);
+    // If i == 8, pawn promotion. Not yet implemented.
+    if (this.isWhite) {
+      this.addSquareIfEmpty(board, this.i - 1, this.j);
+      if (this.firstMove) {
         this.addSquareIfEmpty(board, this.i - 2, this.j);
-      }
-      else {
-        this.addSquareIfEmpty(board, this.i + 1, this.j);
-        this.addSquareIfEmpty(board, this.i + 2, this.j);
       }
     }
     else {
-      if (this.isWhite) {
-        this.addSquareIfEmpty(board, this.i - 1, this.j);
-      }
-      else {
-        this.addSquareIfEmpty(board, this.i + 1, this.j);
+      this.addSquareIfEmpty(board, this.i + 1, this.j);
+      if (this.firstMove) {
+        this.addSquareIfEmpty(board, this.i + 2, this.j);
       }
     }
+
     this.checkDiagonalCaptures(board);
   }
 
