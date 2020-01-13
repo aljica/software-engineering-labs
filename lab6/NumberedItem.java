4732b484611dc8177a1ba892ab60f5fdde166872
aljica <aljica@debian>
2020-01-13 22:16:16 +0100

    done part 1

---
+++ lab6/NumberedItem.java
@@ -0,0 +1,31 @@
+class NumberedItem<T> {
+
+  private T item;
+  private int index;
+
+  public NumberedItem(int index, T item) {
+    this.item = item;
+    if (index < 0) this.index = 0;
+    else this.index = index;
+  }
+
+  public int getIndex() {
+    return this.index;
+  }
+
+  public <T> int compareTo(NumberedItem<T> o) {
+    return this.getIndex() - o.getIndex();
+  }
+
+  public <T> boolean equals(NumberedItem<T> o) {
+    if (o == null) return false;
+
+    if (this.getIndex() - o.getIndex() == 0) return true;
+    return false;
+  }
+
+  public String toString() {
+    return "Index: " + this.index + " Value: " + this.item;
+  }
+
+}
