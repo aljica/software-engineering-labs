e867b84596a496e0f981498b4263873eeaa9f2b9
aljica <aljica@debian>
2020-01-14 15:40:11 +0100

    work on vecelement

---
+++ lab6/Main.java
@@ -0,0 +1,9 @@
+class Main {
+
+  public static void main(String[] u) {
+
+    VecElement<Integer> v = new VecElement<>(0, 2);
+
+  }
+
+}
--- lab6/VecElement.java
+++ lab6/VecElement.java
@@ -1,14 +1,63 @@
 import java.util.*;
 
-class VecElement<E extends Comparable<E>> implements SparseVec<E extends Comparable<E>> {
+class VecElement<E extends Comparable<E>> implements SparseVec<E> {
 
   private int index;
   private E item;
-  private TreeMap<K,V> tree = new TreeMap<K,V>();
+  private static TreeMap<Integer, Object> tree = new TreeMap<>(); // Object? Questionable.
 
   public VecElement(int index, E item) {
     this.index = index;
     this.item = item;
+  }
+
+  @Override
+  public void add(E elem) {
+    return;
+  }
+
+  public void add(int pos, E elem) {
+    return;
+  }
+
+  public int indexOf(E elem) {
+    return 0;
+  }
+
+  public void removeAt(int pos) {
+    return;
+  }
+
+  public void removeElem(E elem) {
+    return;
+  }
+
+  public int size() {
+    return 0;
+  }
+
+  public int minIndex() {
+    return 0;
+  }
+
+  public int maxIndex() {
+    return 0;
+  }
+
+  public String toString() {
+    return "";
+  }
+
+  public E get(int pos) {
+    return null;
+  }
+
+  public Object[] toArray() {
+    return null;
+  }
+
+  public List<E> sortedValues() {
+    return null;
   }
 
 }
